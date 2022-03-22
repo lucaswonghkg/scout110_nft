b09a9524e3fbc037ffc57f670d1b9b2dd229e408
lucaswonghkg <lucaswonghkg@gmail.com>
2022-03-23 02:06:20 +0800

    powerbartext

--- js/power_bar_text.js
+++ js/power_bar_text.js
@@ -1,19 +1,19 @@
 var timer_sec = 1;
 
 var text_value = document.getElementById("land_percentage");
-text_value.innerHTML = "少於10%土地已被開發";
+text_value.innerHTML = "只有16%土地已被開發";
 
 function timer() {
     timer_sec = timer_sec + 1
 
     if (timer_sec % 2 == 0 ){
 
-        text_value.innerHTML = "Developed Land <10%";
+        text_value.innerHTML = "Developed Land 16%";
     }
     
     else {
     
-        text_value.innerHTML = "少於10%土地已被開發";  
+        text_value.innerHTML = "只有16%土地已被開發";  
     }
       
 
