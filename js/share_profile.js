eccabe400c73c22df5f293ae7fe666ad19684891
lucaswonghkg <lucaswonghkg@gmail.com>
2022-03-23 21:05:07 +0800

    shareupdates4

--- js/share_profile.js
+++ js/share_profile.js
@@ -3,9 +3,17 @@
 // } 
 var os = "unknown"
 
-if (isMobile.any()){
+if( navigator.userAgent.match(/iPhone/i)
+|| navigator.userAgent.match(/webOS/i)
+|| navigator.userAgent.match(/Android/i)
+|| navigator.userAgent.match(/iPad/i)
+|| navigator.userAgent.match(/iPod/i)
+|| navigator.userAgent.match(/BlackBerry/i)
+|| navigator.userAgent.match(/Windows Phone/i)
+)
+{
  os = "mobile"
-}
+ }
 
 console.log(os)
 
